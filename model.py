bab330f29069d2baf80bbec1d933758787842269
JulesBelveze <jules.belveze@hotmail.fr>
2020-06-05 11:40:17 +0200

    chore: add models

---
+++ model.py
@@ -0,0 +1,173 @@
+import numpy as np
+import torch
+from torch import nn
+from torch.autograd import Variable
+from torch.nn import functional as tf
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+def init_hidden(x, hidden_size: int, num_dir: int = 1, xavier: bool = True):
+    if xavier:
+        return nn.init.xavier_normal_(torch.zeros(num_dir, x.size(0), hidden_size)).to(device)
+    return Variable(torch.zeros(num_dir, x.size(0), hidden_size)).to(device)
+
+
+###########################################################################
+################################ ENCODERS #################################
+###########################################################################
+
+class Encoder(nn.Module):
+    def __init__(self, config, input_size: int):
+        super(Encoder, self).__init__()
+        self.input_size = input_size
+        self.hidden_size = config['hidden_size_encoder']
+        self.seq_len = config['seq_len']
+        self.lstm = nn.LSTM(input_size=input_size, hidden_size=config['hidden_size_encoder'])
+
+    def forward(self, input_data):
+        h_t, c_t = (init_hidden(input_data, self.hidden_size),
+                    init_hidden(input_data, self.hidden_size))
+        input_encoded = Variable(torch.zeros(input_data.size(0), self.seq_len, self.hidden_size))
+
+        for t in range(self.seq_len):
+            _, (h_t, c_t) = self.lstm(input_data[:, t, :].unsqueeze(0), (h_t, c_t))
+            input_encoded[:, t, :] = h_t
+        return _, input_encoded
+
+
+class AttnEncoder(nn.Module):
+    def __init__(self, config, input_size: int):
+        super(AttnEncoder, self).__init__()
+        self.input_size = input_size
+        self.hidden_size = config['hidden_size_encoder']
+        self.seq_len = config['seq_len']
+        self.add_noise = config['denoising']
+        self.lstm = nn.LSTM(
+            input_size=self.input_size,
+            hidden_size=self.hidden_size,
+            num_layers=1
+        )
+        self.attn = nn.Linear(
+            in_features=2 * self.hidden_size + self.seq_len,
+            out_features=1
+        )
+        self.softmax = nn.Softmax(dim=1)
+
+    def _get_noise(self, input_data, sigma=0.01, p=0.1):
+        normal = sigma * torch.randn(input_data.shape)
+        mask = np.random.uniform(size=(input_data.shape))
+        mask = (mask < p).astype(int)
+        noise = normal * torch.tensor(mask)
+        return noise
+
+    def forward(self, input_data):
+        h_t, c_t = (init_hidden(input_data, self.hidden_size, num_dir=self.directions),
+                    init_hidden(input_data, self.hidden_size, num_dir=self.directions))
+
+        attentions, input_encoded = (Variable(torch.zeros(input_data.size(0), self.seq_len, self.input_size)),
+                                     Variable(torch.zeros(input_data.size(0), self.seq_len, self.hidden_size)))
+
+        if self.add_noise and self.training:
+            input_data += self._get_noise(input_data).to(device)
+
+        for t in range(self.seq_len):
+            x = torch.cat((h_t.repeat(self.input_size, 1, 1).permute(1, 0, 2),
+                           c_t.repeat(self.input_size, 1, 1).permute(1, 0, 2),
+                           input_data.permute(0, 2, 1).to(device)), dim=2).to(
+                device)  # bs * input_size * (2 * hidden_dim + seq_len)
+
+            e_t = self.attn(x.view(-1, self.hidden_size * 2 + self.seq_len))  # (bs * input_size) * 1
+            a_t = self.softmax(e_t.view(-1, self.input_size)).to(device)  # (bs, input_size)
+
+            weighted_input = torch.mul(a_t, input_data[:, t, :].to(device))  # (bs * input_size)
+            self.lstm.flatten_parameters()
+            _, (h_t, c_t) = self.lstm(weighted_input.unsqueeze(0), (h_t, c_t))
+
+            input_encoded[:, t, :] = h_t
+            attentions[:, t, :] = a_t
+
+        return attentions, input_encoded
+
+
+###########################################################################
+################################ DECODERS #################################
+###########################################################################
+
+class Decoder(nn.Module):
+    def __init__(self, config):
+        super(Decoder, self).__init__()
+        self.seq_len = config['seq_len']
+        self.hidden_size = config['hidden_size_decoder']
+        self.lstm = nn.LSTM(1, config['hidden_size_decoder'], bidirectional=False)
+        self.fc = nn.Linear(config['hidden_size_decoder'], config['output_size'])
+
+    def forward(self, _, y_hist):
+        h_t, c_t = (init_hidden(y_hist, self.hidden_size),
+                    init_hidden(y_hist, self.hidden_size))
+
+        for t in range(self.seq_len):
+            inp = y_hist[:, t].unsqueeze(0).unsqueeze(2)
+            lstm_out, (h_t, c_t) = self.lstm(inp, (h_t, c_t))
+        return self.fc(lstm_out.squeeze(0))
+
+
+class AttnDecoder(nn.Module):
+    def __init__(self, config):
+        super(AttnDecoder, self).__init__()
+        self.seq_len = config['seq_len']
+        self.encoder_hidden_size = config['hidden_size_encoder']
+        self.decoder_hidden_size = config['hidden_size_decoder']
+        self.out_feats = config['output_size']
+
+        self.attn = nn.Sequential(
+            nn.Linear(2 * self.decoder_hidden_size + self.encoder_hidden_size, self.encoder_hidden_size),
+            nn.Tanh(),
+            nn.Linear(self.encoder_hidden_size, 1)
+        )
+        self.lstm = nn.LSTM(input_size=self.out_feats, hidden_size=self.decoder_hidden_size)
+        self.fc = nn.Linear(self.encoder_hidden_size + self.out_feats, self.out_feats)
+        self.fc_out = nn.Linear(self.decoder_hidden_size + self.encoder_hidden_size, self.out_feats)
+        self.fc.weight.data.normal_()
+
+    def forward(self, input_encoded, y_history):
+        h_t, c_t = (
+            init_hidden(input_encoded, self.decoder_hidden_size), init_hidden(input_encoded, self.decoder_hidden_size))
+        context = Variable(torch.zeros(input_encoded.size(0), self.encoder_hidden_size))
+
+        for t in range(self.seq_len):
+            x = torch.cat((h_t.repeat(self.seq_len, 1, 1).permute(1, 0, 2),
+                           c_t.repeat(self.seq_len, 1, 1).permute(1, 0, 2),
+                           input_encoded.to(device)), dim=2)
+
+            x = tf.softmax(
+                self.attn(
+                    x.view(-1, 2 * self.decoder_hidden_size + self.encoder_hidden_size)
+                ).view(-1, self.seq_len),
+                dim=1)
+
+            context = torch.bmm(x.unsqueeze(1), input_encoded.to(device))[:, 0, :]  # (batch_size, encoder_hidden_size)
+
+            y_tilde = self.fc(torch.cat((context.to(device), y_history[:, t].unsqueeze(1).to(device)),
+                                        dim=1))  # (batch_size, out_size)
+
+            self.lstm.flatten_parameters()
+            _, (h_t, c_t) = self.lstm(y_tilde.unsqueeze(0), (h_t, c_t))
+
+        return self.fc_out(torch.cat((h_t[0], context.to(device)), dim=1))  # predicting value at t=self.seq_length+1
+
+
+class AutoEncForecast(nn.Module):
+    def __init__(self, config, input_size):
+        super(AutoEncForecast, self).__init__()
+        self.encoder = AttnEncoder(config, input_size).to(device) if config['input_att'] else \
+            Encoder(config, input_size).to(device)
+        self.decoder = AttnDecoder(config).to(device) if config['temporal_att'] else Decoder(config).to(device)
+
+    def forward(self, encoder_input, y_hist, return_attention: bool = True):
+        attentions, encoder_output = self.encoder(encoder_input)
+        outputs = self.decoder(encoder_output, y_hist)
+
+        if return_attention:
+            return outputs, attentions
+        return outputs
