02f9bfef83f0a0247e8175a6f708bc1f77eaf011
JulesBelveze <jules.belveze@hotmail.fr>
2020-06-05 12:34:42 +0200

    chore: update flags

--- main.py
+++ main.py
@@ -12,23 +12,36 @@ from eval import evaluate
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--batch-size", default=config["batch_size"], type=int)
-    parser.add_argument("--output-size", default=config["output_size"], type=int)
-    parser.add_argument("--label-col", default=config["label_col"])
-    parser.add_argument("--input-att", default=config["input_att"], type=lambda x: (str(x).lower() == "true"))
-    parser.add_argument("--temporal-att", default=config["temporal_att"], type=lambda x: (str(x).lower() == "true"))
-    parser.add_argument("--seq-len", default=config["seq_len"], type=int)
-    parser.add_argument("--hidden-size-encoder", default=config["hidden_size_encoder"], type=int)
-    parser.add_argument("--hidden-size-decoder", default=config["hidden_size_decoder"], type=int)
-    parser.add_argument("--reg-factor1", default=config["reg_factor1"], type=float)
-    parser.add_argument("--reg-factor2", default=config["reg_factor2"], type=float)
-    parser.add_argument("--reg1", default=config["reg1"], type=lambda x: (str(x).lower() == "true"))
-    parser.add_argument("--reg2", default=config["reg2"], type=lambda x: (str(x).lower() == "true"))
-    parser.add_argument("--do-train", default=False, type=lambda x: (str(x).lower() == "true"))
-    parser.add_argument("--do-eval", default=False, type=lambda x: (str(x).lower() == "true"))
-    parser.add_argument("--data-path", default="data.csv")
-    parser.add_argument("--output-dir", default=config["output_dir"])
-    parser.add_argument("--ckpt", default=None)
+    parser.add_argument("--batch-size", default=config["batch_size"], type=int, help="batch size")
+    parser.add_argument("--output-size", default=config["output_size"], type=int,
+                        help="size of the ouput: default value to 1 for forecasting")
+    parser.add_argument("--label-col", default=config["label_col"], type=str, help="name of the target column")
+    parser.add_argument("--input-att", default=config["input_att"], type=lambda x: (str(x).lower() == "true"),
+                        help="whether or not activate the input attention mechanism")
+    parser.add_argument("--temporal-att", default=config["temporal_att"], type=lambda x: (str(x).lower() == "true"),
+                        help="whether or not activate the temporal attention mechanism")
+    parser.add_argument("--seq-len", default=config["seq_len"], type=int, help="window length to use for forecasting")
+    parser.add_argument("--hidden-size-encoder", default=config["hidden_size_encoder"], type=int,
+                        help="size of the encoder's hidden states")
+    parser.add_argument("--hidden-size-decoder", default=config["hidden_size_decoder"], type=int,
+                        help="size of the decoder's hidden states")
+    parser.add_argument("--reg-factor1", default=config["reg_factor1"], type=float,
+                        help="contribution factor of the L1 regularization if using a sparse autoencoder")
+    parser.add_argument("--reg-factor2", default=config["reg_factor2"], type=float,
+                        help="contribution factor of the L2 regularization if using a sparse autoencoder")
+    parser.add_argument("--reg1", default=config["reg1"], type=lambda x: (str(x).lower() == "true"),
+                        help="activate/deactivate L1 regularization")
+    parser.add_argument("--reg2", default=config["reg2"], type=lambda x: (str(x).lower() == "true"),
+                        help="activate/deactivate L2 regularization")
+    parser.add_argument("--denoising", default=config["denoising"], type=lambda x: (str(x).lower() == "true"),
+                        help="whether or not to use a denoising autoencoder")
+    parser.add_argument("--do-train", default=False, type=lambda x: (str(x).lower() == "true"),
+                        help="whether or not to train the model")
+    parser.add_argument("--do-eval", default=False, type=lambda x: (str(x).lower() == "true"),
+                        help="whether or not evaluating the mode")
+    parser.add_argument("--data-path", default="data.csv", help="path to data file")
+    parser.add_argument("--output-dir", default=config["output_dir"], help="name of folder to output files")
+    parser.add_argument("--ckpt", default=None, help="checkpoint path for evaluation")
     return parser.parse_args()
 
 
