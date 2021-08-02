0e730b7d0061065cd0927293a6d9ef741459a90d
JulesBelveze <jules.belveze@hotmail.fr>
2021-08-02 20:45:07 +0200

    fix: target scale

--- tsa/dataset.py
+++ tsa/dataset.py
@@ -7,47 +7,50 @@ from torch.utils.data import TensorDataset, DataLoader
 
 class TimeSeriesDataset(object):
     def __init__(self, data, categorical_cols, target_col, seq_length, prediction_window=1):
-        '''
+        """
         :param data: dataset of type pandas.DataFrame
         :param categorical_cols: name of the categorical columns, if None pass empty list
         :param target_col: name of the targeted column
         :param seq_length: window length to use
         :param prediction_window: window length to predict
-        '''
+        """
         self.data = data
         self.categorical_cols = categorical_cols
         self.numerical_cols = list(set(data.columns) - set(categorical_cols) - set(target_col))
         self.target_col = target_col
         self.seq_length = seq_length
         self.prediction_window = prediction_window
-        self.preprocessor = None
-
-    def preprocess_data(self):
-        '''Preprocessing function'''
-        X = self.data.drop(self.target_col, axis=1)
-        y = self.data[self.target_col]
 
         self.preprocessor = ColumnTransformer(
             [("scaler", StandardScaler(), self.numerical_cols),
              ("encoder", OneHotEncoder(), self.categorical_cols)],
             remainder="passthrough"
         )
+        if self.target_col:
+            self.y_scaler = StandardScaler()
+
+    def preprocess_data(self):
+        """Preprocessing function"""
+        X = self.data.drop(self.target_col, axis=1)
+        y = self.data[self.target_col]
 
         X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.8, shuffle=False)
         X_train = self.preprocessor.fit_transform(X_train)
         X_test = self.preprocessor.transform(X_test)
 
         if self.target_col:
-            return X_train, X_test, y_train.values, y_test.values
+            y_train = self.y_scaler.fit_transform(y_train)
+            y_test = self.y_scaler.transform(y_test)
+            return X_train, X_test, y_train, y_test
         return X_train, X_test
 
     def frame_series(self, X, y=None):
-        '''
+        """
         Function used to prepare the data for time series prediction
         :param X: set of features
         :param y: targeted value to predict
         :return: TensorDataset
-        '''
+        """
         nb_obs, nb_features = X.shape
         features, target, y_hist = [], [], []
 
@@ -69,11 +72,11 @@ class TimeSeriesDataset(object):
         return TensorDataset(features_var)
 
     def get_loaders(self, batch_size: int):
-        '''
+        """
         Preprocess and frame the dataset
         :param batch_size: batch size
         :return: DataLoaders associated to training and testing data
-        '''
+        """
         X_train, X_test, y_train, y_test = self.preprocess_data()
         nb_features = X_train.shape[1]
 
@@ -83,3 +86,9 @@ class TimeSeriesDataset(object):
         train_iter = DataLoader(train_dataset, batch_size=batch_size, shuffle=False, drop_last=True)
         test_iter = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, drop_last=True)
         return train_iter, test_iter, nb_features
+
+    def invert_scale(self, predictions):
+        if isinstance(predictions, torch.Tensor):
+            predictions = predictions.numpy()
+        unscaled = self.y_scaler.inverse_transform(predictions)
+        return torch.Tensor(unscaled)
